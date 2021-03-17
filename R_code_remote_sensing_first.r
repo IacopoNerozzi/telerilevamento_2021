a75f34e1cbf5544ed8a3f543ae14483b6089edde
Iacopo Nerozzi <80535446+IacopoNerozzi@users.noreply.github.com>
2021-03-17 12:52:48 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -11,3 +11,16 @@ p224r63_2011
 
 plot(p224r63_2011)
 #visualizzo l'immagine p224r63_2011
+
+#cambio il range di colori
+cl <- colorRampPalette(c("black","grey","light grey")) (100)
+#colori identificati da etichette
+#colori ordinati dal più scuro al più chiaro
+#colori raggruppati in un vettore, diversi elementi per un solo argomento, il colore
+# (100) identifica il numero di livelli dei colori, va esterno alla funzione
+
+plot(p224r63_2011, col=cl)
+
+cl <- colorRampPalette(c("blue","green", "grey","magenta", "yellow")) (100)
+
+plot(p224r63_2011, col=cl)
