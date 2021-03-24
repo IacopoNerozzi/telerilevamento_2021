6606e14aeaa43f43e4295f9e4ca03324502c4095
Iacopo Nerozzi <80535446+IacopoNerozzi@users.noreply.github.com>
2021-03-24 12:56:57 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -5,6 +5,9 @@ setwd("C:/lab/")
 #install.packages("raster")
 library(raster)
 
+#inserisco la mia immagine satellitare
+# utilizzo le virgolette perchè attingo dall'esterno l'informazione
+#la funzione brick la fa associare al nome p224r63_2011
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 p224r63_2011
 #ottengo il contenuto di p224r63_2011
@@ -27,3 +30,72 @@ cl <- colorRampPalette(c("blue","green", "grey","magenta", "yellow")) (100)
 
 plot(p224r63_2011, col=cl)
 #visualizzo l'immagine p224r63_2011
+
+#DAY 3
+
+#Bande Landset
+#B1: blu
+#B2: verde
+#B3: rosso
+#B4: infrarosso vicino
+#B5: infrarosso medio
+#B6: infrarosso termico
+#B7: infrarosso medio
+
+# dev.off ripulirà la finestra grafica, facendo ripartire le informazioni da zerp
+dev.off()
+
+cl <- colorRampPalette(c("red","pink","orange","purple")) (200)
+
+#$ lega due oggetti/blocchi
+plot(p224r63_2011$B1_sre, col=cl)
+#plot band1 con una predefinita banda di colori
+
+#dev.off>()
+#1 riga e una colonna
+> par(mfrow=c(1,2))
+> plot(p224r63_2011$B1_sre)
+> plot(p224r63_2011$B2_sre)
+
+# 2 righe e una colonna
+> par(mfrow=c(2,1))
+> plot(p224r63_2011$B1_sre)
+> plot(p224r63_2011$B2_sre)
+> 
+#specifico prima la colonna
+par(mfcol=c(1,2))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+#plot delle prime 4 bande di Landsat
+par(mfrow=c(4,1))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre)
+plot(p224r63_2011$B4_sre)
+
+# a quadrat of bands...:
+par(mfrow=c(2,2))
+
+ 
+
+clb <- colorRampPalette(c("dark blue","blue","light blue")) (100)
+plot(p224r63_2011$B1_sre, col=clb)
+
+ 
+
+clg <- colorRampPalette(c("dark green","green","light green")) (100)
+plot(p224r63_2011$B2_sre, col=clg)
+
+ 
+
+clr <- colorRampPalette(c("dark red","red","pink")) (100)
+plot(p224r63_2011$B3_sre, col=clr)
+
+ 
+
+clnir <- colorRampPalette(c("red","orange","yellow")) (100)
+plot(p224r63_2011$B4_sre, col=clnir)
+
+
+
